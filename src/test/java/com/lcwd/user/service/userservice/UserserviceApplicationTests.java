cb47631d9a7b48a3a5af2b04322000a3b8a6a8a0
chakraborty89subhajit <47492058+chakraborty89subhajit@users.noreply.github.com>
2026-09-16 09:54:21 +0530

    userservice pom and application.yaml updated successfully

--- src/test/java/com/lcwd/user/service/userservice/UserserviceApplicationTests.java
+++ src/test/java/com/lcwd/user/service/userservice/UserserviceApplicationTests.java
@@ -16,6 +16,8 @@ class UserserviceApplicationTests {
 	void contextLoads() {
 	}
 
+
+	//post method
 	@Test
 	public void createRating(){
 		Rating rating = Rating.builder()
@@ -27,6 +29,31 @@ class UserserviceApplicationTests {
 
 		Rating savedRating = retingService.createRating(rating);
 		System.out.println("new rating service created by feign client");
+	}
+
+	//put method
+	@Test
+	public void updateRating(){
+
+		String ratingId= "9b8df4fb-89b1-47ca-a00a-c9574bc059cb";
+
+		Rating rating = Rating.builder()
+				.rating(7)
+				.userId("")
+				.hotelId("")
+				.feedback("this is updated by feign client")
+				.build();
+
+		Rating updateRating= retingService.updateRating(ratingId,rating);
+		System.out.println("rating service updated by feign client");
+	}
+	//delete method
+	public void deleteRating(){
+		String ratingId = ""; // Pass existing rating ID here
+
+		retingService.deleteRating(ratingId);
+		System.out.println("Rating deleted successfully for ID: " + ratingId);
+
 	}
 
 }
